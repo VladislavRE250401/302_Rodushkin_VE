d14787e63e300021c5034ccf0500af7a5115a950
VladislavRE250401 <vladislavdohrenaumnyj7268@gmail.com>
2022-04-09 12:54:53 +0300

    add Task05

---
+++ Task04/src/StackInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rodushkinve\Task04;
+
+interface StackInterface
+{
+    public function push(...$Array);
+    public function pop();
+    public function top();
+    public function isEmpty();
+    public function copy();
+    public function __toString();
+}
---
+++ Task05/src/run_test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rodushkinve\Task05;
+
+require __DIR__ . '/../vendor/autoload.php';
+use Rodushkinve\Task05\Truncater;
+
+#require 'Truncater.php'; require 'Test.php';
+
+runTest();
