a4f048eea84bc0402b7c9c8bc9276e7a7138bffc
VladislavRE250401 <vladislavdohrenaumnyj7268@gmail.com>
2022-05-21 12:36:23 +0300

    add Task08

---
+++ Task08/excel.ps1
@@ -0,0 +1,12 @@
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
+$ExcelObj = New-Object -COM Excel.Application
+$ExcelWorkBook = $ExcelObj.Workbooks.Add()
+$ExcelWorkSheet = $ExcelWorkBook.Worksheets.Item(1)
+$ExcelWorkSheet.Cells.Item(2,2) = "Привет от PowerShell"
+$ExcelWorkSheet.Cells.Item(2,2).Font.Italic = $true
+$ExcelWorkSheet.Cells.Item(2,2).Font.size=12
+$ExcelWorkSheet.Columns.Item(2).ColumnWidth=25
+$excelWorkBook.SaveAs("$desktopPath\"+$env:UserName+"_"+$env:ComputerName+".xlsx")
+$ExcelWorkBook.close($true)
+pause
